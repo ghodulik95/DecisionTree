8fe5adf45a4d7118c5b79b75da1c751d68736d82
George Hodulik <gmh73@case.edu>
2015-09-15 19:01:36 -0700

    Added some stuff to dtree

--- dtree.py
+++ dtree.py
@@ -14,11 +14,30 @@ class DecisionTree(object):
         @param depth=None : maximum depth of the tree,
                             or None for no maximum depth
         """
+        self.treeHead = TreeNode(None, None, None)
+        self.maxDepth = depth
         pass
 
     def fit(self, X, y, sample_weight=None):
         """ Build a decision tree classifier trained on data (X, y) """
-        pass
+        numTotal = len(y)
+        numPositive = len(filter(lambda l: l == 1, y))
+        if numPositive == numTotal:
+            self.treeHead.classLabelConfidence = 1.0
+            return
+        else if numPositive == 0:
+            self.treeHead.classLabelConfidence = 0.0
+            return
+        else if len(X) == 0 or len(X[0]) == 0
+            self.treeHead.classLabelConfidence = numPositive/numTotal
+            return
+
+        ID3(self.treeHead, X, y, range(numTotal), range(len(X[0])))
+
+        return
+
+    def ID3(root, X, y, indexes, attributes):
+        
 
     def predict(self, X):
         """ Return the -1/1 predictions of the decision tree """
@@ -40,4 +59,31 @@ class DecisionTree(object):
         (A tree with a single root node has depth 0)
         """
         pass
+
+    def entropyY(y):
+        totalNum = len(y)
+        numPositive = len(filter(lambda l: l == 1, y))
+        numNegative = len(filter(lambda l: l != 1, y))
+
+        pp = numPositive/totalNum
+        pn = numNegative/totalNum
+
+        return - (pp*np.log2(pp) + pn*np.log2(pn)) 
+
+    def entropyYgivenX(X, y):
+        pass
+
+
+class TreeNode(object)
+
+    def __init__(self, parent, trueNode=None, falseNode=None):
+        self.trueNode = trueNode
+        self.falseNode = falseNode
+        self.parent = parent
+        self.classLabelConfidence = None
+
+    def makeLeafNode(self, confidenceLevel)
+        self.classLabelConfidence = confidenceLevel
+        self.trueNode = None
+        self.falseNode = None
 
