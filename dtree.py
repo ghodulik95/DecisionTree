e8f4424f330fdf880ee6a44abfb52de1876ea994
George Hodulik <ghodulik95@gmail.com>
2015-09-19 21:01:22 -0400

    Wrote everthing - have not tested.

--- dtree.py
+++ dtree.py
@@ -20,24 +20,69 @@ class DecisionTree(object):
 
     def fit(self, X, y, sample_weight=None):
         """ Build a decision tree classifier trained on data (X, y) """
-        numTotal = len(y)
-        numPositive = len(filter(lambda l: l == 1, y))
-        if numPositive == numTotal:
-            self.treeHead.classLabelConfidence = 1.0
-            return
-        else if numPositive == 0:
-            self.treeHead.classLabelConfidence = 0.0
-            return
-        else if len(X) == 0 or len(X[0]) == 0
-            self.treeHead.classLabelConfidence = numPositive/numTotal
-            return
+        
 
-        ID3(self.treeHead, X, y, range(numTotal), range(len(X[0])))
+        ID3(self.treeHead, X, y, range(len(y)), range(len(X[0])))
 
         return
 
     def ID3(root, X, y, indexes, attributes):
+        numTotal = len(indexes)
+        numPositive = len(filter(lambda l: l == 1, y))
+        if numPositive == numTotal:
+            root.classLabelConfidence = 1.0
+            return
+        else if numPositive == 0:
+            root.classLabelConfidence = 0.0
+            return
+        else if len(indexes) == 0 or len(attributes) == 0
+            root.classLabelConfidence = numPositive/numTotal
+            return
+
+        bestAttr = getBestAttr(X, y, indexes, attributes)
+        root.attribute = bestAttr
+        for val in getValuesOf(bestAttr, X)
+            childNodeWithVal = TreeNode(root)
+            root.children.append(childNodeWithVal)
+            indexesWithVal = filter(lambda l: X[l][attr] == val, indexes)
+            if len(indexesWithVal) == 0:
+                childNodeWithVal.classLabelConfidence = numPositive/numTotal
+            else:
+                ID3(childNodeWithVal, X, y, indexesWithVal, attributes.remove(bestAttr))
+
+        return
         
+    def getBestAttriute(X, y, indexes, attributes):
+        numTotal = len(indexes)
+        bestAttr = -1
+        lowestEntropy = float("inf")
+        for attr in attributes:
+            entropy = 0.0
+            possibleValues = getValuesOf(attr, X)
+            for val in possibleValues:
+                indexesWithVal = filter(lambda l: X[l][attr] == val, indexes)
+                numYPositive = filter(lambda l: y[l] == 1, indexesWithVal)
+                entropy += calcEntropy(numYPositive, len(indexesWithVal), numTotal)
+            if entropy < lowestEntropy:
+                lowestEntropy = entropy
+                bestAttr = attr
+
+        return bestAttr
+
+    def calcEntropy(numPositive, numWithVal, numTotal):
+         if(numPositive == 0 || numPositive == numTotal):
+            return 0
+
+         probVal = numWithVal / numTotal
+         probPos = numPositive / numWithVal
+         probNeg = 1 - probPos
+         return - (probPos*np.log2(probPos) + probNeg*np.log2(probNeg))
+
+    def getValuesOf(attr, X):
+        vals = set()
+        for example in X:
+            vals.add(example[attr])
+        return vals
 
     def predict(self, X):
         """ Return the -1/1 predictions of the decision tree """
@@ -76,9 +121,8 @@ class DecisionTree(object):
 
 class TreeNode(object)
 
-    def __init__(self, parent, trueNode=None, falseNode=None):
-        self.trueNode = trueNode
-        self.falseNode = falseNode
+    def __init__(self, parent):
+        self.children = []
         self.parent = parent
         self.classLabelConfidence = None
 
